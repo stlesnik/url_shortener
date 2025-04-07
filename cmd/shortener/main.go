047ea812dbca8d0afd8548d9ee2a81e99fbf3374
20680015 <AAShchupak@sberbank.ru>
2025-04-07 18:49:42 +0300

    panics to log.Fatal migration

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	cfg, err := config.NewConfig()
 	if err != nil {
-		panic(err)
+		log.Fatalf("Не получилось создать конфиг: %s", err)
+		return
 	}
 	repo := repository.NewInMemoryRepository()
 	srv := server.NewServer(repo, cfg)
@@ -18,6 +19,7 @@ func main() {
 	log.Printf("Сервер запущен на %s", cfg.ServerAddress)
 	err = srv.Start()
 	if err != nil {
-		panic(err)
+		log.Fatalf("Не получилось запустить сервер: %s", err)
+		return
 	}
 }
